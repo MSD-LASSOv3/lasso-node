1ffdc202509270ebf4aa5ee761baed96b845f3f3
Matthew Krebs <mlk6450@rit.edu>
2022-01-17 16:26:22 -0500

    fixed GPS setup bugs

--- src/setup/gps-setup.sh
+++ src/setup/gps-setup.sh
@@ -7,8 +7,8 @@ bash -c "echo 'init_uart_baud=9600' >> /boot/config.txt"
 
 bash -c "echo 'pps-gpio' >> /etc/modules"
 
-sed -i "s/GPSD_OPTIONS=\"\"/GPSD_OPTIONS=/\"-n\"" /etc/default/gpsd
-sed -i "s/DEVICES=\"\"/DEVICES=\"/dev/ttyS0 /dev/pps0\"" /etc/default/gpsd
+sed -i "s%GPSD_OPTIONS=\"\"%GPSD_OPTIONS=\"-n\"%" /etc/default/gpsd
+sed -i "s%DEVICES=\"\"%DEVICES=\"/dev/ttyS0/dev/pps0\"%" /etc/default/gpsd
 
 echo "refclock SHM 0 refid NMEA offset 0.200" >> /etc/chrony/chrony.conf
 echo "refclock PPS /dev/pps0 refid PPS lock NMEA" >> /etc/chrony/chrony.conf
