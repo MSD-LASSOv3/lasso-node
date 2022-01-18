4dc6f03945a3d375f55cfd1fca3fdd5465e5403c
Matthew Krebs <mlk6450@rit.edu>
2022-01-17 20:38:30 -0500

    strange bug fix that wasn't needed when I used the raspberry imager utility to format the boot sd card

--- src/remote/auto-ssh.sh
+++ src/remote/auto-ssh.sh
@@ -2,13 +2,15 @@
 createTunnel() {
 	autossh -M 10984 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /home/pi/.ssh/id_rsa -R $1:localhost:22 lasso-node@lasso.rit.edu -p 22 &
 	if [[ $? -eq 0 ]]; then
-		echo Tunnel to Server created successfully
+		echo "Tunnel to Server created successfully"
 	else
-		echo An error occured creating a tunnel to server. RC was $?
+		echo "An error occured creating a tunnel to server. RC was $?"
 	fi
 }
-/usr/bin/pidof ssh
-if [[ $? -ne 0 ]]; then
+
+if [[ $(pidof sshd) -ne 0 ]]; then
 	echo "Creating a new tunnel connection using port $1"
 	createTunnel $1
+else
+	echo "Error: SSH not running. Unable to make tunnel"
 fi
