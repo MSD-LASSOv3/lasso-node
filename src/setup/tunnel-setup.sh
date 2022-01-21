7a002af3386935dbe6597b067d3a392f68c4bf6f
Matthew Krebs <mlk6450@rit.edu>
2022-01-21 11:23:45 -0500

    added native autossh loging

--- src/setup/tunnel-setup.sh
+++ src/setup/tunnel-setup.sh
@@ -11,5 +11,9 @@ sudo -u pi ssh-keygen -t rsa -q -f "/home/pi/.ssh/id_rsa" -N ""
 # share the node's public key with the server and server's public key with the node
 sudo -u pi cat /home/pi/.ssh/id_rsa.pub | ssh lasso-node@lasso.rit.edu 'cat >> ~/.ssh/authorized_keys; cat ~/.ssh/id_rsa.pub' | cat >> /home/pi/.ssh/authorized_keys
 
+# enable built in autossh loging
+echo >> /home/pi/.bashrc
+echo "export AUTOSSH_LOGFILE=\"\$HOME/autossh.log\"" >> /home/pi/.bashrc
+
 # add a cron job to create a reverse ssh tunnel from the node to the server on startup
 { crontab -l -u pi; echo "*/1 * * * * ${src}/remote/auto-ssh.sh $1 > ${src}/../tunnel.log 2>&1"; } | crontab -u pi -
