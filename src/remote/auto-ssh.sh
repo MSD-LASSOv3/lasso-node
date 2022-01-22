54dad11d5332291f53d3f2aeb916bc06ff31e28b
Matthew Krebs <mlk6450@rit.edu>
2022-01-22 15:43:43 -0500

    added extra check to confirm tunnel is setup. Unsure what would happen if the server-side authorized_hosts is not properly configured

--- src/remote/auto-ssh.sh
+++ src/remote/auto-ssh.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+IP=lasso.rit.edu
+
 createTunnel() {
-	autossh -M 10984 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /home/pi/.ssh/id_rsa -R $1:localhost:22 lasso-node@lasso.rit.edu -p 22 &
+	autossh -M 10984 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /home/pi/.ssh/id_rsa -R $1:localhost:22 lasso-node@$IP -p 22 &
 	if [[ $? -eq 0 ]]; then
-		echo "SUCCESS"
+		if [[ -n $(ssh lasso-node@$IP "netstat -l | grep $1") ]]; then
+			echo "SUCCESS"
+		else
+			echo "WARNING: unable to verify connection"
+		fi
 	else
 		echo "FAILURE: RC was $?"
 	fi
