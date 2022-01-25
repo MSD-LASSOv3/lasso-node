24fd821768bb24a329bcf206ab8ba0d94ba60cae
Matthew Krebs <mlk6450@rit.edu>
2022-01-25 14:23:36 -0500

    proposed solution to gps setup bugs

--- src/setup/gps-setup.sh
+++ src/setup/gps-setup.sh
@@ -7,11 +7,16 @@ bash -c "echo 'init_uart_baud=9600' >> /boot/config.txt"
 
 bash -c "echo 'pps-gpio' >> /etc/modules"
 
+bash -c "echo START_DAEMON=\"true\" >> /etc/default/gpsd"
 sed -i "s%GPSD_OPTIONS=\"\"%GPSD_OPTIONS=\"-n\"%" /etc/default/gpsd
-sed -i "s%DEVICES=\"\"%DEVICES=\"/dev/ttyS0/dev/pps0\"%" /etc/default/gpsd
+sed -i "s%DEVICES=\"\"%DEVICES=\"/dev/ttyS0 /dev/pps0\"%" /etc/default/gpsd
 
 echo "refclock SHM 0 refid NMEA offset 0.200" >> /etc/chrony/chrony.conf
 echo "refclock PPS /dev/pps0 refid PPS lock NMEA" >> /etc/chrony/chrony.conf
+
+systemctl disable serial-getty@ttys0.service
+systemctl stop serial-getty@ttys0.service
+systemctl restart gpsd
 
 #to test PPS do
 # lsmod | grep pps
